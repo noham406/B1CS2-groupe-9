41a72f1e7b5b91a7dba65e14706606989afc0bd5
“Rmartinuzzi” <raphael.martinuzzi@efrei.net>
2025-04-15 10:18:40 +0200

    J'ajoute le dossier pages

---
+++ pages/bahrein.php
@@ -0,0 +1,233 @@
+<main>
+    <div class="left">
+        <div class="track">
+            <img src="images/tracks/bahrein.jpg" alt="bahrain's track">
+        </div>
+        <div class="teams_results">
+            <table border="1">
+                <thead>
+                    <tr>
+                        <th>Position</th>
+                        <th>Ecurie</th>
+                        <th>Points</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>RedBull</td>
+                        <td>44</td>
+                    </tr>
+
+                    <tr>
+                        <td>2</td>
+                        <td>Ferrari</td>
+                        <td>27</td>
+                    </tr>
+
+                    <tr>
+                        <td>3</td>
+                        <td>Mercedes</td>
+                        <td>16</td>
+                    </tr>
+
+                    <tr>
+                        <td>4</td>
+                        <td>McLaren</td>
+                        <td>12</td>
+                    </tr>
+
+                    <tr>
+                        <td>5</td>
+                        <td>Aston Martin</td>
+                        <td>3</td>
+                    </tr>
+
+                    <tr>
+                        <td>6</td>
+                        <td>Alpine</td>
+                        <td>0</td>
+                    </tr>
+
+                    <tr>
+                        <td>7</td>
+                        <td>Racing Bulls</td>
+                        <td>0</td>
+                    </tr>
+
+                    <tr>
+                        <td>8</td>
+                        <td>Williams</td>
+                        <td>0</td>
+                    </tr>
+
+                    <tr>
+                        <td>9</td>
+                        <td>Haas</td>
+                        <td>0</td>
+                    </tr>
+
+                    <tr>
+                        <td>10</td>
+                        <td>Stake Sauber</td>
+                        <td>0</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <div class="drivers_results">
+        <table border="1">
+            <thead>
+                <tr>
+                    <th>Position</th>
+                    <th>Pilote</th>
+                    <th>Ecurie</th>
+                    <th>Points</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>&nbsp;🇳🇱&nbsp;&nbsp;Max Verstappen</td>
+                    <td>Redbull</td>
+                    <td>25</td>
+                </tr>
+
+                <tr>
+                    <td>2</td>
+                    <td>&nbsp;🇲🇽&nbsp;&nbsp;Sergio Perez</td>
+                    <td>RedBull</td>
+                    <td>18</td>
+                </tr>
+
+                <tr>
+                    <td>3</td>
+                    <td>&nbsp;🇪🇸&nbsp;&nbsp;Carlos Sainz</td>
+                    <td>Ferrari</td>
+                    <td>15</td>
+                </tr>
+
+                <tr>
+                    <td>4</td>
+                    <td>&nbsp;🇲🇨&nbsp;&nbsp;Charles Leclerc</td>
+                    <td>Ferrari</td>
+                    <td>12</td>
+                </tr>
+
+                <tr>
+                    <td>5</td>
+                    <td>&nbsp;🇬🇧&nbsp;&nbsp;George Russell</td>
+                    <td>Mercedes</td>
+                    <td>10</td>
+                </tr>
+
+                <tr>
+                    <td>6</td>
+                    <td>&nbsp;🇬🇧&nbsp;&nbsp;Lando Norris</td>
+                    <td>McLaren</td>
+                    <td>8</td>
+                </tr>
+                
+                <tr>
+                    <td>7</td>
+                    <td>&nbsp;🇬🇧&nbsp;&nbsp;Lewis Hamilton</td>
+                    <td>Mercedes</td>
+                    <td>6</td>
+                </tr>
+                
+                <tr>
+                    <td>8</td>
+                    <td>&nbsp;🇦🇺&nbsp;&nbsp;Oscar Piastri</td>
+                    <td>McLaren</td>
+                    <td>4</td>
+                </tr>
+                
+                <tr>
+                    <td>9</td>
+                    <td>&nbsp;🇪🇸&nbsp;&nbsp;Fernando Alonso</td>
+                    <td>Aston Martin</td>
+                    <td>2</td>
+                </tr>
+                
+                <tr>
+                    <td>10</td>
+                    <td>&nbsp;🇨🇦&nbsp;&nbsp;Lance Stroll</td>
+                    <td>Aston Martin</td>
+                    <td>1</td>
+                </tr>
+                
+                <tr>
+                    <td>11</td>
+                    <td>&nbsp;🇨🇳&nbsp;&nbsp;Zhou Guanyu</td>
+                    <td>Stake Sauber</td>
+                    <td>0</td>
+                </tr>
+                
+                <tr>
+                    <td>12</td>
+                    <td>&nbsp;🇩🇰&nbsp;&nbsp;Kevin Magnussen</td>
+                    <td>Haas</td>
+                    <td>0</td>
+                </tr>
+                
+                <tr>
+                    <td>13</td>
+                    <td>&nbsp;🇦🇺&nbsp;&nbsp;Daniel Ricciardo</td>
+                    <td>Racing Bulls</td>
+                    <td>0</td>
+                </tr>
+                
+                <tr>
+                    <td>14</td>
+                    <td>&nbsp;🇯🇵&nbsp;&nbsp;Yuki Tsunoda</td>
+                    <td>Racing Bulls</td>
+                    <td>0</td>
+                </tr>
+                
+                <tr>
+                    <td>15</td>
+                    <td>&nbsp;🇹🇭&nbsp;&nbsp;Alexander Albon</td>
+                    <td>Williams</td>
+                    <td>0</td>
+                </tr>
+                
+                <tr>
+                    <td>16</td>
+                    <td>&nbsp;🇩🇪&nbsp;&nbsp;Nico Hulkenberg</td>
+                    <td>Haas</td>
+                    <td>0</td>
+                </tr>
+                
+                <tr>
+                    <td>17</td>
+                    <td>&nbsp;🇫🇷&nbsp;&nbsp;Esteban Ocon</td>
+                    <td>Alpine</td>
+                    <td>0</td>
+                </tr>
+                
+                <tr>
+                    <td>18</td>
+                    <td>&nbsp;🇫🇷&nbsp;&nbsp;Pierre Gasly</td>
+                    <td>Alpine</td>
+                    <td>0</td>
+                </tr>
+                
+                <tr>
+                    <td>19</td>
+                    <td>&nbsp;🇫🇮&nbsp;&nbsp;Valtteri Bottas</td>
+                    <td>Stake Sauber</td>
+                    <td>0</td>
+                </tr>
+                
+                <tr>
+                    <td>20</td>
+                    <td>&nbsp;🇺🇸&nbsp;&nbsp;Logan Sargeant</td>
+                    <td>Williams</td>
+                    <td>0</td>
+                </tr>
+                
+            </tbody>
+        </table>
+    </div>
+</main>
