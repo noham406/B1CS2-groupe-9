10bda3abedf2b065bb050acec7cdbe89f0d42b9b
lubin-lokaj <lubin.lokaj@gmail.com>
2025-04-16 22:54:35 +0200

    Envoi du dossier des includes, avec notamment la clé de connexion à la base de données (localhost)

---
+++ includes/bottom_form.php
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+    function confirmSuppression() {
+        return confirm("Êtes-vous sûr de vouloir supprimer cet enregistrement ?");
+    }
+</script>
+</body>
+</html>
---
+++ includes/top_form.php
@@ -0,0 +1,36 @@
+<?php
+define('BASE', '/projet_s2/');
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>
+        <?php
+            if(isset($title)){
+                echo $title;
+            } else {
+                echo "Gestion DB";
+            }
+        ?>
+    </title>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= BASE ?>css/style_admin.css">
+</head>
+<body>
+    <header>
+        <h1>
+            <?php
+                if(isset($titre)){
+                    echo $titre;
+                } else {
+                    echo "Gestion de la Base de données";
+                }
+            ?>
+        </h1>
+    </header>
+    <nav>
+        <a href="<?= BASE ?>">Accueil</a>
+        <a href="<?= BASE ?>private/admin.php">Accueil Admin</a>
+    </nav>
