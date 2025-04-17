23320b20dca53dbba1cd982ede16879e8429405f
lubin-lokaj <lubin.lokaj@gmail.com>
2025-04-17 11:07:55 +0200

    Page administrateur avec accès à tous les formulaires

--- private/admin.php
+++ private/admin.php
@@ -1,27 +1,15 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <title>Admin</title>
-    <link rel="stylesheet" href="../css/style_admin.css">
-</head>
-<body>
-    <header>
-        <h1>Gestion de la base de données</h1>
-    </header>
-    <nav>
-        <a href="../index.php">Accueil</a>
-        <a href="admin.php">Accueil Admin</a>
-    </nav>
+<?php
+$title = 'Admin';
+$titre = 'Gestion de la base de données';
+include '../includes/top_form.php';
+?>
     <main>
         <section class="enregistrer">
             <h2>Enregistrer des données</h2>
-            <a href="gp.php">GP</a>
-            <a href="ecuries.php">Ecuries</a>
-            <a href="pilotes.php">Pilotes</a>
-            <a href="resultats.php">Resultats</a>
+            <a href="gp.php">Grand Prix</a>
+            <a href="ecuries.php">Ecurie</a>
+            <a href="pilotes.php">Pilote</a>
+            <a href="resultats.php">Resultats Grand Prix</a>
             <a href="resultats_sprint.php">Resultats Sprint</a>
         </section>
         <section class="consulter">
@@ -30,11 +18,13 @@
             <a href="affichage_ecuries.php">Ecuries</a>
             <a href="affichage_pilotes.php">Pilotes</a>
             <a href="affichage_res_gp.php">Résultats Grand Prix</a>
-            <!-- Afficher les résultats des courses sprint, et mettre le classement dynamique autre part, peut être en dessous -->
+            <a href="affichage_res_sprint.php">Résultats Sprint</a>
+        </section>
+        <section id="admin">
+            <h2>Classement Dynamique</h2>
             <a href="classement.php">Classement Dynamique</a>
         </section>
     </main>
-</body>
-</html>
+<?php include '../includes/bottom_form.php';
 
 
