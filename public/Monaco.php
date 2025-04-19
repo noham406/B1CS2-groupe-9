4b30eb0b950f02f35d9d16e81e0a34a1f0dbce1c
lubin-lokaj <lubin.lokaj@gmail.com>
2025-04-19 09:34:36 +0200

    Ajout des titres

--- public/Monaco.php
+++ public/Monaco.php
@@ -9,6 +9,7 @@ $gpid = 8
         <img src="images/tracks/monaco.jpg" alt="Monaco's track">
     </div>
     <div class="teams_results">
+        <h2>Resultats des écuries</h2>
         <table border="1">
             <thead>
                 <tr>
@@ -47,6 +48,7 @@ $gpid = 8
 
 <!-- Résultats par pilote - Monaco -->
 <div class="drivers_results">
+    <h2>Resultats des pilotes</h2>
     <table border="1">
         <thead>
             <tr>
