6899f1fd2cfd39a62e257b54dfb51c3f2213952a
lubin-lokaj <lubin.lokaj@gmail.com>
2025-04-19 09:35:11 +0200

    Ajout des titres

--- public/Pays Bas.php
+++ public/Pays Bas.php
@@ -9,6 +9,7 @@ $gpid = 15
         <img src="images/tracks/zandvoort.jpg" alt="Pays bas's track">
     </div>
     <div class="teams_results">
+        <h2>Resultats des écuries</h2>
         <table border="1">
             <thead>
                 <tr>
@@ -47,6 +48,7 @@ $gpid = 15
 
 <!-- Résultats par pilote - Pays bas -->
 <div class="drivers_results">
+    <h2>Resultats des pilotes</h2>
     <table border="1">
         <thead>
             <tr>
