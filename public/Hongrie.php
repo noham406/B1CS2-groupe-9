4462b4056620389b8aa652152c9fff4206cbf8e9
lubin-lokaj <lubin.lokaj@gmail.com>
2025-04-19 09:29:54 +0200

    Ajout des titres

--- public/Hongrie.php
+++ public/Hongrie.php
@@ -9,6 +9,7 @@ $gpid = 13
         <img src="images/tracks/hungary.jpg" alt="Hongrie's track">
     </div>
     <div class="teams_results">
+        <h2>Resultats des écuries</h2>
         <table border="1">
             <thead>
                 <tr>
@@ -47,6 +48,7 @@ $gpid = 13
 
 <!-- Résultats par pilote - Hongrie -->
 <div class="drivers_results">
+    <h2>Resultats des pilotes</h2>
     <table border="1">
         <thead>
             <tr>
